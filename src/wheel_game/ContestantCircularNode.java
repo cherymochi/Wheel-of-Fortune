9ee67b019e67e4e84666656ad3ec6368af957e4e
Kyle Williams <jigwillams8@gmail.com>
2023-04-07 17:48:49 -0500

    Finished the node class

---
+++ src/wheel_game/ContestantCircularNode.java
@@ -0,0 +1,45 @@
+package wheel_game;
+
+// Programmer : Kyle Williams
+// Date : Mar 28, 2023
+// ContestantCircularNode Class
+
+public class ContestantCircularNode {
+
+    // Attributes
+    private Contestant contestantInfo;
+    private ContestantCircularNode nextNode;
+
+    // Default Constructor
+    ContestantCircularNode(){
+        contestantInfo = new Contestant();
+        nextNode = null;
+    }
+
+    // Copy Constructors
+    public ContestantCircularNode(Contestant contestantInfo) {
+        this.contestantInfo = contestantInfo;
+    }
+
+    public ContestantCircularNode(ContestantCircularNode nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    // Getters
+    public Contestant getContestantInfo() {
+        return contestantInfo;
+    }
+
+    public ContestantCircularNode getNextNode() {
+        return nextNode;
+    }
+
+    // Setters
+    public void setContestantInfo(Contestant contestantInfo) {
+        this.contestantInfo = contestantInfo;
+    }
+
+    public void setNextNode(ContestantCircularNode nextNode) {
+        this.nextNode = nextNode;
+    }
+}
